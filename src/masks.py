1d6e158c1d8f531ed79368d5553688e1640ce72d
duxevy <pechenevskyvladislav@gmail.com>
2026-05-10 11:27:51 +0300

    изменения bar

--- src/masks.py
+++ src/masks.py
@@ -1,9 +1,9 @@
 def foo(a, b):
     """Просто функция"""
-    a = a + 10
+    a = a + 10 + 2
     return a + b
 
 def bar(x, y):
     """Просто функция 2"""
-    a = foo(1, 2) + 20
+    a = foo(1, 2) + 20 + 1
     return a ** 2
